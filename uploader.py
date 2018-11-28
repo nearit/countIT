730c7cd9dcd6edb52039dc3e39453515b1cb4fd6
Federico Boschini <federico@nearit.com>
2018-11-28 18:40:22 +0100

    feat(uploader): define function to upload (un)compressed files to bucket

---
+++ uploader.py
@@ -0,0 +1,12 @@
+import boto3
+from compressor import gzip_file
+
+s3 = boto3.resource('s3')
+
+def upload_file(filename, bucket_name, compress=True, compress_in_memory=False):
+    bucket = s3.Bucket(bucket_name)
+    if compress:
+        with gzip_file(filename, in_memory=compress_in_memory) as zipped_file:
+            bucket.upload_fileobj(zipped_file, filename, {'ContentType': 'text/plain', 'ContentEncoding': 'gzip'})
+    else:
+        s3.meta.client.upload_file(filename, bucket_name, filename)
