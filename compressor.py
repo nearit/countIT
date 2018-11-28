0b5950a09d35110d32f46d83722f5404968b27a1
Federico Boschini <federico@nearit.com>
2018-11-28 18:19:53 +0100

    feat(compression): define function to gzip a file (in memory / on disk)

---
+++ compressor.py
@@ -0,0 +1,15 @@
+from io import BytesIO
+from tempfile import TemporaryFile
+import gzip
+import shutil
+
+def gzip_file(filename, in_memory=True):
+    with open(filename, 'rb') as fp:
+        if in_memory:
+            compressed_fp = BytesIO()
+        else:
+            compressed_fp = TemporaryFile()
+        with gzip.GzipFile(fileobj=compressed_fp, mode='wb') as gz:
+            shutil.copyfileobj(fp, gz)
+        compressed_fp.seek(0)
+        return compressed_fp
