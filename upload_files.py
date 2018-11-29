85cb8093fc12b198de36127a9f9043e95ee3eb9c
Federico Boschini <federico@nearit.com>
2018-11-29 18:23:47 +0100

    fix(upload): fix where to put files on s3

--- upload_files.py
+++ upload_files.py
@@ -21,4 +21,4 @@ with open(config, 'r') as f:
             # construct the full local path
             file_path = os.path.join(path, filename)
             # upload the file
-            upload_file(file_path, bucket_name)
+            upload_file(file_path, bucket_name, folder_name+"/"+filename)
--- uploader.py
+++ uploader.py
@@ -5,7 +5,7 @@ from compressor import gzip_file
 
 s3 = boto3.resource('s3')
 
-def upload_file(filename, bucket_name, compress=True, compress_in_memory=False):
+def upload_file(filename, bucket_name, destination, compress=True, compress_in_memory=False):
     bucket = s3.Bucket(bucket_name)
     if compress:
         with gzip_file(filename, in_memory=compress_in_memory) as zipped_file:
