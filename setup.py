6b90009c24e2047ce359feec63d678a7360307ef
Federico Boschini <federico@nearit.com>
2018-11-30 11:20:01 +0100

    fix(setup): fix command

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from crontab import CronTab
 
 cron = CronTab(user="pi")
-command = os.getcwd()+"/countIT.py"
+command = os.getcwd()+"/countit.py"
 job = cron.new(command=command, comment="run countIT at boot")
 job.every_reboot()
 cron.write()
