468a05c655d4d7ba13422b69fe216d6de0bbe5d0
Federico Boschini <federico@nearit.com>
2018-12-03 14:37:06 +0100

    feat(server): move config stuff + filter trackings on start/end basis

--- server/main.go
+++ server/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -13,38 +14,24 @@ import (
 	"log"
 )
 
-// Config maps a config file to struct
-type Config struct {
-	Bucket   string `json:"bucket_name"`
-	Region   string `json:"region"`
-	Env      string `json:"env"`
-	Customer string `json:"customer"`
-	ID       string `json:"id"`
-}
+const dateFormat = "2006-01-02_15:04:05"
 
 func main() {
 	config := loadConfig("config.json")
-	listObjects(config.Region, config.Bucket, config.Customer+"/"+config.Env+"/"+config.ID)
+	folder := config.Customer + "/" + config.Env + "/" + config.ID
+
+	start, _ := time.Parse(dateFormat, config.StartDate)
+	fmt.Println(start)
+	end, _ := time.Parse(dateFormat, config.EndDate)
+	fmt.Println(end)
+	listObjects(config.Region, config.Bucket, folder, start, end)
 }
 
-func loadConfig(filename string) Config {
-	var config Config
-	configFile, err := os.Open(filename)
-	defer configFile.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
-	return config
-}
-
-func listObjects(region string, bucket string, folder string) {
+func listObjects(region string, bucket string, folder string, start time.Time, end time.Time) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region)},
 	)
 
-	// Create S3 service client
 	svc := s3.New(sess)
 
 	// Get the list of items
@@ -57,11 +44,13 @@ func listObjects(region string, bucket string, folder string) {
 	}
 
 	for _, item := range resp.Contents {
-		fmt.Println("Name:         ", *item.Key)
-		fmt.Println("Last modified:", *item.LastModified)
-		fmt.Println("Size:         ", *item.Size)
-		fmt.Println("Storage class:", *item.StorageClass)
-		fmt.Println("")
+		time, _ := time.Parse(dateFormat, strings.TrimPrefix(*item.Key, folder+"/"))
+		if inTimeSpan(start, end, time) {
+			fmt.Println("Name:         ", *item.Key)
+			fmt.Println("Last modified:", *item.LastModified)
+			fmt.Println("Size:         ", *item.Size)
+			fmt.Println("")
+		}
 	}
 
 	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
@@ -76,26 +65,27 @@ func downloadObject(filename string, region string, bucket string) {
 
 	defer file.Close()
 
-	// 2) Create an AWS session
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String(region)},
 	)
 
-	// 3) Create a new AWS S3 downloader
 	downloader := s3manager.NewDownloader(sess)
 
-	// 4) Download the item from the bucket. If an error occurs, log it and exit. Otherwise, notify the user that the download succeeded.
-	numBytes, err := downloader.Download(file,
+	numBytes, downloadErr := downloader.Download(file,
 		&s3.GetObjectInput{
 			Bucket: aws.String(bucket),
 			Key:    aws.String(filename),
 		})
 
-	if err != nil {
+	if downloadErr != nil {
 		log.Fatalf("Unable to download item %q, %v", filename, err)
 	}
 
 	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+}
+
+func inTimeSpan(start, end, check time.Time) bool {
+	return check.After(start) && check.Before(end)
 }
 
 func exitErrorf(msg string, args ...interface{}) {
