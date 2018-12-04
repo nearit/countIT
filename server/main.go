7b0a484ba0cd50821e5fcec41599b7447de4be2f
Federico Boschini <federico@nearit.com>
2018-12-04 14:46:07 +0100

    refactor(server): add fields to struct + rename func

--- server/main.go
+++ server/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"log"
+	"time"
 
 	"io"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -18,15 +19,16 @@ import (
 
 // Trackings contains total count and a list of detections
 type Trackings struct {
+	From    time.Time   `json:"from"`
+	To      time.Time   `json:"to"`
 	Count   int         `json:"count"`
 	Devices []Detection `json:"devices"`
 }
 
 // Detection contains RSSI, MAC address and Manufaturer detected
 type Detection struct {
-	RSSI         float32 `json:"rssi"`
-	MAC          string  `json:"mac"`
-	Manufacturer string  `json:"Manufacturer"`
+	RSSI float32 `json:"rssi"`
+	MAC  string  `json:"mac"`
 }
 
 var config Config
@@ -36,10 +38,10 @@ func main() {
 	config = loadConfig("config.json")
 	folder = config.Customer + "/" + config.Env + "/" + config.ID
 
-	listObjects()
+	queryBucket()
 }
 
-func listObjects() {
+func queryBucket() {
 	fmt.Println("Fetching detections from", config.StartDate, "to", config.EndDate)
 
 	start := parseDate(config.StartDate)
@@ -80,16 +82,14 @@ func readFile(filename string) Trackings {
 		Key:    aws.String(filename),
 	})
 	if err != nil {
-		//log.Fatalf("Unable to get object %q from bucket %v", filename, err)
-		fmt.Printf("Unable to get object %q from bucket %v", filename, err)
+		log.Fatalf("Unable to get object %q from bucket %v", filename, err)
 	}
 
 	defer results.Body.Close()
 
 	buf := bytes.NewBuffer(nil)
 	if _, err := io.Copy(buf, results.Body); err != nil {
-		//log.Fatalf("Unable to bufferize results, %v", err)
-		fmt.Printf("Unable to bufferize results, %v", err)
+		log.Fatalf("Unable to bufferize results, %v", err)
 	}
 
 	var trackings Trackings
