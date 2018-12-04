6d8275831cca2eed7f782ef2238dbf62f65bd1aa
Federico Boschini <federico@nearit.com>
2018-12-04 16:49:14 +0100

    feat(server): add util func to parse date with custom format

--- server/util.go
+++ server/util.go
@@ -7,6 +7,14 @@ import (
 
 const dateFormat = "2006-01-02 15:04:05 +0100"
 
+func parseDateWithFormat(date, format string) time.Time {
+	parsed, err := time.Parse(format, date)
+	if err != nil {
+		log.Fatalf("Unable to parse date, %q %v", date, err)
+	}
+	return parsed
+}
+
 func parseDate(date string) time.Time {
 	parsed, err := time.Parse(dateFormat, date)
 	if err != nil {
