7a72ce7941da256114daed76a6e1cf56bd963a08
Federico Boschini <federico@nearit.com>
2018-12-07 15:28:57 +0100

    feat(setup): config log file

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+"""Setup: schedules and runs countit and setups logs"""
 import os
 
 from crontab import CronTab
@@ -9,5 +9,8 @@ command = os.getcwd()+"/countit.py"
 job = cron.new(command=command, comment="run countIT at boot")
 job.every_reboot()
 cron.write()
+
+open("/var/log/countit.log", 'a').close()
+os.chmod("/var/log/countit.log", 0o666)
 
 os.system(command)
